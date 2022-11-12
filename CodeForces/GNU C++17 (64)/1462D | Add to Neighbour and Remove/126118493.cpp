bfd6bbc26b2bcc62f6488fc29fadc8229b149adb
Mark Zhou <xinyunzhouu@gmail.com>
2022-11-12 15:24:47 -0500

    Committing files

---
+++ CodeForces/GNU C++17 (64)/1462D | Add to Neighbour and Remove/126118493.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+ 
+int main() {
+ cin.sync_with_stdio(false);
+ cin.tie(nullptr);
+ int tc;
+ cin >> tc;
+ while (tc--) {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  ll sum = 0;
+  for (int &x : a) {
+   cin >> x;
+   sum += x;
+  }
+  int ans = INT_MAX;
+  for (int i = 1; i <= n; i++) {
+   if (sum % i == 0) {
+    bool flag = true;
+    ll req = sum / i, cur = 0;
+    for (int j = 0; j < n; j++) {
+     cur += a[j];
+     if (cur == req) {
+      cur = 0;
+     }
+     if (cur > req) {
+      flag = false;
+     }
+    }
+    if (flag) {
+     ans = n - i;
+    }
+   }
+  }
+  cout << ans << '\n';
+ }
+}
