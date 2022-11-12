6854c31c7ebf21effe4433cfb5150fe81ed56f45
Mark Zhou <xinyunzhouu@gmail.com>
2022-11-12 15:16:00 -0500

    Committing files

---
+++ CodeForces/GNU C++20 (64)/847E | Packmen/143455691.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// * * * P *
+ 
+int main() {
+#ifdef LOCAL
+  freopen("input.in", "r", stdin);
+#endif
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n;
+  string s;
+  cin >> n >> s;
+  vector<int> pac, ast;
+  for (int i = 0; i < n; i++) {
+    if (s[i] == 'P') {
+      pac.push_back(i);
+    }
+    if (s[i] == '*') {
+      ast.push_back(i);
+    }
+  }
+  int psz = pac.size(), asz = ast.size();
+  int l = 0, r = n * 2;
+  while (l + 1 < r) {
+    int mid = l + (r - l) / 2;
+    int j = 0;
+    for (int i = 0; i < psz; i++) {
+      int ldist = max(0, pac[i] - ast[j]);
+      while (j < asz && ast[j] < pac[i] && pac[i] - ast[j] <= mid) {
+        j++;
+      }
+      while (j < asz && ast[j] > pac[i] && ldist + ast[j] - pac[i] + min((ldist == 0 ? INT_MAX : ldist), (ldist == 0 ? 0 : ast[j] - pac[i])) <= mid) {
+        j++;
+      }
+    }
+    (j == asz ? r : l) = mid;
+  }
+  cout << r << '\n';
+}
