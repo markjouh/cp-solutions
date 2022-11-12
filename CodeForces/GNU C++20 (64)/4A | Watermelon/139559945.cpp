8b9f8cf1c4fdc18afda7745ac8d345dd9bc115ce
Mark Zhou <xinyunzhouu@gmail.com>
2022-11-12 15:18:23 -0500

    Committing files

---
+++ CodeForces/GNU C++20 (64)/4A | Watermelon/139559945.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+ ios::sync_with_stdio(false);
+ cin.tie(nullptr); 
+ int w;
+ cin >> w;
+ cout << (w >= 4 && w % 2 == 0 ? "YES" : "NO") << '\n';
+}
+ 
+/*
+ Read the problem statement carefully
+ Try to think of ideas from past problems
+ Check for edge cases, overflow, etc.
+*/
+ 
