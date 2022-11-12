5f389061ba87eda6f2e9112162103f4933ff703f
Mark Zhou <xinyunzhouu@gmail.com>
2022-11-12 15:26:57 -0500

    Committing files

---
+++ CodeForces/GNU C++17 (64)/1371C | A Cookie for You/124051534.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+ cin.sync_with_stdio(0);
+ cin.tie(0);
+ int tc;
+ cin >> tc;
+ while (tc--) {
+  long long a, b, n, m;
+  cin >> a >> b >> n >> m;
+  if (a == b) {
+   a--;
+   m--;
+  }
+  (a < b ? a : b) -= m;
+  cout << (a >= 0 && b >= 0 && a + b >= n ? "Yes" : "No") << '\n';
+ }
+}
