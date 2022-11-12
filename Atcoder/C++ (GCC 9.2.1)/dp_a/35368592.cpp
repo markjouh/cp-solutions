049bb446fcb033c0bb9f2b25fee9408ee439372f
Mark Zhou <xinyunzhouu@gmail.com>
2022-11-12 15:02:09 -0500

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/dp_a/35368592.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+#define all(x) (x).begin(), (x).end()
+#define siz(x) (int)(x).size()
+#define fi first
+#define se second
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+    vector<int> h(n);
+    for (int i = 0; i < n; i++) {
+        cin >> h[i];
+    }
+    vector<int> dp(n, 1e9);
+    dp[0] = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 1; j <= 2; j++) {
+            if (i + j < n) {
+                dp[i + j] = min(dp[i + j], dp[i] + abs(h[i] - h[i + j]));  
+            }
+        }
+    }
+    cout << dp[n - 1] << '\n';
+}
