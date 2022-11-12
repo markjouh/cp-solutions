c8256a6b5b038b345e13d0c981e81c0fbf5793d8
Mark Zhou <xinyunzhouu@gmail.com>
2022-11-12 15:02:37 -0500

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1748A | The Ultimate Square/180617479.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> pt;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        ll n;
+        cin >> n;
+        cout << (n + 1) / 2 << '\n';
+    }
+}
