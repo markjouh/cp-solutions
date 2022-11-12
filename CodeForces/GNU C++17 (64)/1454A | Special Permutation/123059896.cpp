fa5a572520dfbf24b0af827634eb7d66f1557a94
Mark Zhou <xinyunzhouu@gmail.com>
2022-11-12 15:28:29 -0500

    Committing files

---
+++ CodeForces/GNU C++17 (64)/1454A | Special Permutation/123059896.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+ cin.sync_with_stdio(0);
+ cin.tie(0);
+ int t;
+ cin >> t;
+ while (t--) {
+  int n;
+  cin >> n;
+  int a[n];
+  iota(a, a + n, 1);
+  reverse(a, a + n);
+  if (n % 2) {
+   swap(a[0], a[n / 2]);
+  }
+  for (int x : a) {
+   cout << x << ' ';
+  }
+  cout << '\n';
+ }
+}
