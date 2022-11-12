a50d59133d2c0eb48c5ab21e5302f540b812d93e
Mark Zhou <xinyunzhouu@gmail.com>
2022-11-12 15:31:12 -0500

    Committing files

---
+++ CodeForces/GNU C++17 (64)/1375B | Neighbor Grid/120961656.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int arr[300][300];
+ 
+inline int allneighbors(int i, int j, int n, int m) {
+ int res = 0;
+ if (i > 0) res++;
+ if (i < n - 1) res++;
+ if (j > 0) res++;
+ if (j < m - 1) res++;
+ return res;
+}
+ 
+void solve() {
+ int n, m;
+ cin >> n >> m;
+ for (int i = 0; i < n; i++) {
+  for (int j = 0; j < m; j++) {
+   cin >> arr[i][j];
+  }
+ }
+ for (int i = 0; i < n; i++) {
+  for (int j = 0; j < m; j++) {
+   if (arr[i][j] > allneighbors(i, j, n, m)) {
+    cout << "NO" << '\n';
+    return;
+   }
+   arr[i][j] = allneighbors(i, j, n, m);
+  }
+ }
+ cout << "YES" << '\n';
+ for (int i = 0; i < n; i++) {
+  for (int j = 0; j < m; j++) {
+   cout << arr[i][j] << ' ';
+  }
+  cout << '\n';
+ }
+}
+ 
+int main() {
+ int t;
+ cin >> t;
+ while (t--) {
+  solve();
+ }
+}
