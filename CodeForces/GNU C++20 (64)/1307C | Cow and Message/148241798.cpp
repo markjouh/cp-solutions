e9a208526451a9741c5b169bca87b368a5d83c8d
Mark Zhou <xinyunzhouu@gmail.com>
2022-11-12 15:14:10 -0500

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1307C | Cow and Message/148241798.cpp
@@ -0,0 +1,30 @@
+#pragma GCC optimize ("Ofast")
+#pragma GCC optimize ("unroll-loops")
+ 
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  string s;
+  cin >> s;
+  vector<int> ct(26), single(26);
+  vector<vector<long long>> occur(26, vector<long long>(26));
+  for (int i = s.size()-1; i >= 0; i--) {
+    for (int j = 0; j < 26; j++) {
+      occur[s[i]-'a'][j] += ct[j];
+    }
+    ct[s[i]-'a']++;
+    single[s[i]-'a']++;
+  }
+  long long ans = 0;
+  for (int i = 0; i < 26; i++) {
+    for (int j = 0; j < 26; j++) {
+      ans = max(ans, occur[i][j]);
+    }
+    ans = max(ans, (long long)single[i]);
+  }
+  cout << ans << '\n';
+}
