728b8d6d38d04d3d490e5cb94c5708a915d04b2c
Mark Zhou <xinyunzhouu@gmail.com>
2022-11-12 15:14:16 -0500

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1389B | Array Walk/147966949.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// Testing out the speed of getchar
+ 
+inline int read(){
+  int res = 0;
+  char c = getchar();
+  while (c < '0') {
+    c = getchar();
+  }
+  while (c >= '0') {
+    res = (res<<3) + (res<<1) + c-48;
+    c = getchar();
+  }
+  return res;
+}
+ 
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t = read();
+  while (t--) {
+    int n, k, z;
+    n = read();
+    k = read();
+    z = read();
+    vector<int> a(n), pre;
+    for (int &x : a) {
+      x = read();
+    }
+    pre = a;
+    for (int i = 1; i < n; i++) {
+      pre[i] += pre[i-1];
+    }
+    long long ans = 0;
+    for (int i = 1; i < n; i++) {
+      if (i > k) {
+        continue;
+      }
+      int ck = k-i, l = min(z, (ck+1)/2), r = min(l, ck/2);
+      long long cur = pre[i] + a[i-1]*l + a[i]*r;
+      if (ck-l-r) {
+        cur += pre[min(n-1, i+ck-l-r)] - pre[i];
+      }
+      ans = max(ans, cur);
+    }
+    cout << ans << '\n';
+  }
+}
