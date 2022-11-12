88878754d8d74fa59ada0c6d07aa1f9912bcf2bc
Mark Zhou <xinyunzhouu@gmail.com>
2022-11-12 15:03:36 -0500

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1738B | Prefix Sum Addicts/177929236.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#ifdef MARK
+#define DEBUG(...) debug(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define DEBUG(...)
+#endif
+ 
+template<typename T, typename S> ostream& operator << (ostream &os, const pair<T, S> &p) {return os << "(" << p.first << ", " << p.second << ")";}
+template<typename C, typename T = decay<decltype(*begin(declval<C>()))>, typename enable_if<!is_same<C, string>::value>::type* = nullptr> ostream& operator << (ostream &os, const C &c) {bool f = true; os << "["; for (const auto &x : c) {if (!f) os << ", "; f = false; os << x;} return os << "]";}
+template<typename T> void debug(string s, T x) {cerr << "\033[1;35m" << s << "\033[0;32m = \033[33m" << x << "\033[0m\n";}
+template<typename T, typename... Args> void debug(string s, T x, Args... args) {for (int i=0, b=0; i<(int)s.size(); i++) if (s[i] == '(' || s[i] == '{') b++; else if (s[i] == ')' || s[i] == '}') b--; else if (s[i] == ',' && b == 0) {cerr << "\033[1;35m" << s.substr(0, i) << "\033[0;32m = \033[33m" << x << "\033[31m | "; debug(s.substr(s.find_first_not_of(' ', i + 1)), args...); break;}}
+ 
+typedef long long ll;
+ 
+#define all(x) (x).begin(), (x).end()
+#define siz(x) (int)(x).size()
+#define fi first
+#define se second
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        ll n, k;
+        cin >> n >> k;
+        vector<ll> p(k);
+        for (int i = 0; i < k; i++) {
+            cin >> p[i];
+        }
+        bool ok = true;
+        ll prev = 2e18;
+        for (int i = k - 1; i >= 1; i--) {
+            ll d = p[i] - p[i - 1];
+            if (d > prev) {
+                ok = false;
+                break;
+            }
+            prev = d;
+        }
+        if (k > 1 && (n - k + 1) * prev < p[0]) {
+            ok = false;
+        }
+        cout << (ok ? "YES\n" : "NO\n");
+    }
+}
