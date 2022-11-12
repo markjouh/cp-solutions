0d99fce46d276e32eeb0ac60058a1537c2be3758
Mark Zhou <xinyunzhouu@gmail.com>
2022-11-12 15:27:34 -0500

    Committing files

---
+++ CodeForces/GNU C++17 (64)/1450A | Avoid Trygub/123637044.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+ cin.sync_with_stdio(0);
+ cin.tie(0);
+ int t;
+ cin >> t;
+ while (t--) {
+  int n;
+  string s;
+  cin >> n >> s;
+  sort(s.begin(), s.end());
+  cout << s << '\n';
+ }
+}
