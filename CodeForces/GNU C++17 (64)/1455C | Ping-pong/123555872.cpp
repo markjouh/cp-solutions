fdb75bd6b4527232691381578eb175226bd9fa0d
Mark Zhou <xinyunzhouu@gmail.com>
2022-11-12 15:27:44 -0500

    Committing files

---
+++ CodeForces/GNU C++17 (64)/1455C | Ping-pong/123555872.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+ cin.sync_with_stdio(0);
+ cin.tie(0);
+ int t;
+ cin >> t;
+ while (t--) {
+  int x, y;
+  cin >> x >> y;
+  cout << x - 1 << ' ' << y << '\n';
+ }
+}
