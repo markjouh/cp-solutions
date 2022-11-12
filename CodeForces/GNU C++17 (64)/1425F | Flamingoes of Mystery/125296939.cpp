7b27173281760214a68a681c60d9ba4830711768
Mark Zhou <xinyunzhouu@gmail.com>
2022-11-12 15:25:32 -0500

    Committing files

---
+++ CodeForces/GNU C++17 (64)/1425F | Flamingoes of Mystery/125296939.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+ 
+int main() {
+ auto query = [&](int a, int b) {
+  cout << '?' << ' ' << a << ' ' << b << endl;
+  int x;
+  cin >> x;
+  return x;
+ };
+ int n;
+ cin >> n;
+ vector<int> pre(n + 1);
+ for (int i = 2; i <= n; i++) {
+  pre[i] = query(1, i);
+ }
+ pre[1] = pre[n] - query(2, n);
+ cout << '!' << ' ';
+ for (int i = 1; i <= n; i++) {
+  cout << pre[i] - pre[i - 1] << ' ';
+ }
+ cout << endl;
+}
