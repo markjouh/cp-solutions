1414c8389d4013b0e554b153a4f8a77a8b6221c8
Mark Zhou <xinyunzhouu@gmail.com>
2022-11-12 15:02:04 -0500

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/dp_g/35454837.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+#define all(x) (x).begin(), (x).end()
+#define siz(x) (int)(x).size()
+#define fi first
+#define se second
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> g(n);
+    vector<int> in(n);
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        cin >> u >> v;
+        u--; v--;
+        g[u].push_back(v);
+        in[v]++;
+    }
+
+    queue<int> q;
+    for (int i = 0; i < n; i++) {
+        if (in[i] == 0) {
+            q.push(i);
+        }
+    }
+    vector<int> o;
+    while (siz(q)) {
+        int u = q.front();
+        q.pop();
+        o.push_back(u);
+        for (int v : g[u]) {
+            if (--in[v] == 0) {
+                q.push(v);
+            }
+        }
+    }
+
+    vector<int> dp(n);
+    for (int u : o) {
+        for (int v : g[u]) {
+            dp[v] = max(dp[v], dp[u] + 1);
+        }
+    }
+
+    cout << *max_element(all(dp)) << '\n';
+}
