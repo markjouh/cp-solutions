71b1cfd9618f6b20795f93af62d23beac30ee798
Mark Zhou <xinyunzhouu@gmail.com>
2022-11-12 15:04:16 -0500

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1743B | Permutation Value/176710813.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long long ll;
+ 
+#define all(x) (x).begin(), (x).end()
+#define siz(x) (int)(x).size()
+#define fi first
+#define se second
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int n;
+        cin >> n;
+        cout << 1 << ' ';
+        for (int i = n; i > 1; i--) {
+            cout << i << ' ';
+        }
+        cout << '\n';
+    }
+}
