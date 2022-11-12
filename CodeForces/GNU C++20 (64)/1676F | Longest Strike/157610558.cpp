bfd08cc3c982d4a0f8958a7f10cfba76c53549e3
Mark Zhou <xinyunzhouu@gmail.com>
2022-11-12 15:10:23 -0500

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1676F | Longest Strike/157610558.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#ifdef LOCAL
+#define DEBUG(...) debug(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define DEBUG(...)
+#endif
+ 
+template<typename T, typename S> ostream& operator << (ostream &os, const pair<T, S> &p) {return os << "(" << p.first << ", " << p.second << ")";}
+template<typename C, typename T = decay<decltype(*begin(declval<C>()))>, typename enable_if<!is_same<C, string>::value>::type* = nullptr>
+ostream& operator << (ostream &os, const C &c) {bool f = true; os << "["; for (const auto &x : c) {if (!f) os << ", "; f = false; os << x;} return os << "]";}
+template<typename T> void debug(string s, T x) {cerr << "\033[1;35m" << s << "\033[0;32m = \033[33m" << x << "\033[0m\n";}
+template<typename T, typename... Args> void debug(string s, T x, Args... args) {for (int i=0, b=0; i<(int)s.size(); i++) if (s[i] == '(' || s[i] == '{') b++; else
+if (s[i] == ')' || s[i] == '}') b--; else if (s[i] == ',' && b == 0) {cerr << "\033[1;35m" << s.substr(0, i) << "\033[0;32m = \033[33m" << x << "\033[31m | ";debug(s.substr(s.find_first_not_of(' ', i + 1)), args...); break;}}
+ 
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int tc;
+  cin >> tc;
+  while (tc--) {
+    int n, k;
+    cin >> n >> k;
+    map<int, int> cnt;
+    for (int i = 0; i < n; i++) {
+      int x;
+      cin >> x;
+      cnt[x]++;
+    }
+    vector<int> a;
+    for (auto [f, s] : cnt) {
+      if (s >= k) {
+        a.push_back(f);
+      }
+    }
+    if (a.empty()) {
+      cout << -1 << '\n';
+      continue;
+    }
+    int mxlen = 1, curlen = 1, l = a[0];
+    int ansl = a[0], ansr = a[0];
+    for (int i = 1; i < (int)a.size(); i++) {
+      if (a[i] != a[i - 1] + 1) {
+        curlen = 0;
+        l = a[i];
+      }
+      curlen++;
+      if (curlen > mxlen) {
+        mxlen = curlen;
+        ansl = l;
+        ansr = a[i];
+      }
+    }
+    cout << ansl << ' ' << ansr << '\n';
+  }
+}
