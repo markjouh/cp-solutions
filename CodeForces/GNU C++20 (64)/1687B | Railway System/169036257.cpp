8a625a43b70df4ea01f8641e740af6f6ecd922e3
Mark Zhou <xinyunzhouu@gmail.com>
2022-11-12 15:05:32 -0500

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1687B | Railway System/169036257.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long long ll;
+ 
+#define all(x) (x).begin(), (x).end()
+#define fi first
+#define se second
+ 
+string s;
+ 
+int ask() {
+    cout << "? " << s << endl;
+    int x;
+    cin >> x;
+    return x;
+}
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n, m;
+    cin >> n >> m;
+    s = string(m, '0');
+    vector<pair<int, int>> e(m);
+    for (int i = 0; i < m; i++) {
+        s[i] = '1';
+        e[i] = {ask(), i};
+        s[i] = '0';
+    }
+    sort(all(e));
+ 
+    int prev = 0;
+    int ans = 0;
+    for (auto [w, i] : e) {
+        s[i] = '1';
+        int cur = ask();
+        if (cur == prev + w) {
+            ans += w;
+            prev = cur;
+        } else {
+            s[i] = '0';
+        }
+    }
+    cout << "! " << ans << endl;
+}
