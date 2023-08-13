4b96a58d5f2fa839fd5fbb29d385e4c6a4ff0527
Mark Zhou <xinyunzhouu@gmail.com>
2023-08-13 13:17:30 -0400

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1843A | Sasha and Array Coloring/212248143.cpp
@@ -0,0 +1,55 @@
+#include "bits/stdc++.h"
+#pragma GCC optimize ("O3")
+#pragma GCC target ("sse4")
+ 
+using namespace std;
+ 
+using LL = long long;
+using PII = pair<int, int>;
+template<class T> using VE = vector<T>;
+template<class T, size_t SZ> using AR = array<T, SZ>;
+ 
+#define FOR(i, A, B) for (int i = A; i < B; i++)
+#define F0R(i, N) FOR(i, 0, N)
+#define RFOR(i, A, B) for (int i = B - 1; i >= A; i--)
+#define RF0R(i, N) RFOR(i, 0, N)
+#define REP(N) F0R(_, N)
+ 
+#define SZ(X) ((int)(X).size())
+#define ALL(X) (X).begin(), (X).end()
+#define RALL(X) (X).rbegin(), (X).rend()
+#define F first
+#define S second
+ 
+template<typename T> static constexpr T inf = numeric_limits<T>::max() / 2;
+template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+ 
+void solve() {
+    int N;
+    cin >> N;
+    VE<int> A(N);
+    F0R(i, N) {
+        cin >> A[i];
+    }
+    sort(ALL(A));
+    int ans = 0;
+    F0R(i, N / 2) {
+        ans += A[N - i - 1] - A[i];
+    }
+    cout << ans << '\n';
+}
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int T = 1;
+    cin >> T;
+    while (T--) {
+        solve();
+    }
+}
+ 
+// Read the statement carefully, take your time
+// Don't be intimidated, the solution is probably simpler than you think
+// CM soon :pray:
