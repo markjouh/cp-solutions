af15a91cf0e7d4260b97317d62cbacb9c0cc2288
Mark Zhou <xinyunzhouu@gmail.com>
2023-08-13 13:14:00 -0400

    Committing files

---
+++ CodeForces/GNU C++20 (64)/158A | Next Round/215856175.cpp
@@ -0,0 +1,36 @@
+#include "bits/stdc++.h"
+using namespace std;
+ 
+typedef long long ll;
+typedef pair<int, int> pii;
+ 
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
+ 
+template<class T> bool ckmin(T &a, const T &b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool ckmax(T &a, const T &b) { return b > a ? a = b, 1 : 0; }
+ 
+void solve() {
+  int n, k;
+  cin >> n >> k;
+  vector<int> a(n);
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
+  }
+  int ans = 0;
+  for (int i = 0; i < n; i++) {
+    ans += a[i] > 0 && a[i] >= a[k - 1];
+  }
+  cout << ans << '\n';
+}
+ 
+int main() {
+  cin.tie(0)->sync_with_stdio(0);
+  cin.exceptions(cin.failbit);
+  int t = 1;
+  // cin >> t;
+  while (t--) {
+    solve();
+  }
+  return 0;
+}
