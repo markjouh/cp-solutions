0fc309e6d76521500b40875829120f9dd9ffa4b5
Mark Zhou <xinyunzhouu@gmail.com>
2023-08-13 13:16:20 -0400

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1844A | Subtraction Game/213299936.cpp
@@ -0,0 +1,17 @@
+#include "bits/stdc++.h"
+ 
+using namespace std;
+ 
+#define int long long
+ 
+int32_t main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  int tc;
+  cin >> tc;
+  while (tc--) {
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << '\n';
+  }
+}
