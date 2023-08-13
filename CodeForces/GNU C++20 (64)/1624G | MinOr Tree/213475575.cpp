35cf6630619141546ea022025080ad16b754dae0
Mark Zhou <xinyunzhouu@gmail.com>
2023-08-13 13:15:59 -0400

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1624G | MinOr Tree/213475575.cpp
@@ -0,0 +1,60 @@
+#include "bits/stdc++.h"
+#pragma GCC optimize ("O3")
+#pragma GCC target ("sse4")
+ 
+using namespace std;
+ 
+constexpr int MXN = 2e5;
+array<int, MXN> par;
+int comps;
+ 
+int get_root(int u) {
+  if (par[u] == -1) {
+    return u;
+  }
+  return par[u] = get_root(par[u]);
+}
+ 
+void unite(int a, int b) {
+  a = get_root(a);
+  b = get_root(b);
+  if (a == b) {
+    return;
+  }
+  if (rand() % 2) {
+    swap(a, b);
+  }
+  par[b] = a;
+  comps--;
+}
+ 
+int32_t main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  int tc;
+  cin >> tc;
+  while (tc--) {
+    int n, m;
+    cin >> n >> m;
+    vector<array<int, 3>> edges(m);
+    for (auto &[u, v, w] : edges) {
+      cin >> u >> v >> w;
+      u--; v--;
+    }
+    int ans = INT_MAX - (1 << 30);
+    for (int i = 29; i >= 0; i--) {
+      ans ^= (1 << i);
+      fill(par.begin(), par.begin() + n, -1);
+      comps = n;
+      for (auto [u, v, w] : edges) {
+        if (ans == (ans | w)) {
+          unite(u, v);
+        }
+      }
+      if (comps > 1) {
+        ans ^= (1 << i);
+      }
+    }
+    cout << ans << '\n';
+  }
+}
