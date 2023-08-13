1a0a13285763aae80d4f93134ff5a9068494c98e
Mark Zhou <xinyunzhouu@gmail.com>
2023-08-13 13:15:55 -0400

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1624G | MinOr Tree/213478174.cpp
@@ -0,0 +1,62 @@
+#include "bits/stdc++.h"
+#pragma GCC optimize ("O3")
+#pragma GCC target ("sse4")
+ 
+using namespace std;
+ 
+constexpr int MXN = 2e5;
+int par[MXN], sz[MXN];
+int edges[MXN][3];
+int comps;
+ 
+int get_root(int u) {
+  if (par[u] == -1) {
+    return u;
+  }
+  return par[u] = get_root(par[u]);
+}
+ 
+void unite(int a, int b) {
+  a = get_root(a);
+  b = get_root(b);
+  if (a == b) {
+    return;
+  }
+  if (sz[a] < sz[b]) {
+    swap(a, b);
+  }
+  par[b] = a;
+  sz[a] += sz[b];
+  comps--;
+}
+ 
+int32_t main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  int tc;
+  cin >> tc;
+  while (tc--) {
+    int n, m;
+    cin >> n >> m;
+    for (int i = 0; i < m; i++) {
+      cin >> edges[i][0] >> edges[i][1] >> edges[i][2];
+      edges[i][0]--; edges[i][1]--;
+    }
+    int ans = INT_MAX - (1 << 30);
+    for (int i = 29; i >= 0; i--) {
+      ans ^= (1 << i);
+      fill(par, par + n, -1);
+      fill(sz, sz + n, 1);
+      comps = n;
+      for (int i = 0; i < m; i++) {
+        if (ans == (ans | edges[i][2])) {
+          unite(edges[i][0], edges[i][1]);
+        }
+      }
+      if (comps > 1) {
+        ans ^= (1 << i);
+      }
+    }
+    cout << ans << '\n';
+  }
+}
