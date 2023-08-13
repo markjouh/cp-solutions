a7c2e1d26f7bff741c3426ad63529ef030243fe3
Mark Zhou <xinyunzhouu@gmail.com>
2023-08-13 13:01:23 -0400

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1642B | Power Walking/218044821.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#ifdef LOCAL
+#include "debug.h"
+#else
+#define dbg(x...)
+#endif
+ 
+typedef long long ll;
+ 
+#define sz(x) static_cast<int>(size(x))
+#define all(x) begin(x), end(x)
+ 
+void solve() {
+    int n;
+    cin >> n;
+    set<int> st;
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        st.insert(x);
+    }
+    for (int i = 1; i <= n; i++) {
+        cout << max(sz(st), i) << ' ';
+    }
+    cout << '\n';
+}
+ 
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+ 
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
+ 
+/*
+ 
+Each distinct value in a must contribute at least 1 to the sum
+The problem is that we may have to break sets of the same value up to be able to give all children at least 1 powerup
+ 
+*/
