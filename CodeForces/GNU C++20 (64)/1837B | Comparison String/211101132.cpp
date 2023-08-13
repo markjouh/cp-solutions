a08a8759854332b7eeaa39ade7a3544ae0ae9b3c
Mark Zhou <xinyunzhouu@gmail.com>
2023-08-13 13:18:25 -0400

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1837B | Comparison String/211101132.cpp
@@ -0,0 +1,69 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <cstdint>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <cstdint>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <vector>
+ 
+using namespace std;
+ 
+#define SZ(X) ((int)(X).size())
+#define ALL(X) (X).begin(), (X).end()
+#define RALL(X) (X).rbegin(), (X).rend()
+#define F first
+#define S second
+ 
+#define VE vector
+#define AR array
+ 
+typedef long long LL;
+typedef pair<int, int> PII;
+ 
+void solve() {
+    int N;
+    string S;
+    cin >> N >> S;
+    int cur = 1, ans = 1;
+    for (int i = 1; i < N; i++) {
+        if (S[i] == S[i - 1]) {
+            cur++;
+        } else {
+            cur = 1;
+        }
+        ans = max(ans, cur);
+    }
+    cout << ans + 1 << '\n';
+}
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int T = 1;
+    cin >> T;
+    while (T--) {
+        solve();
+    }
+}
+ 
+// Read the statement carefully, take your time
+// Don't be intimidated, the solution is probably simpler than you think
+// CM soon :pray:
