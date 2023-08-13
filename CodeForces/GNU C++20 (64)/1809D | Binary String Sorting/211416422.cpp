99997b784530abcad7380241452aa4df112d69fb
Mark Zhou <xinyunzhouu@gmail.com>
2023-08-13 13:17:59 -0400

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1809D | Binary String Sorting/211416422.cpp
@@ -0,0 +1,65 @@
+#include "bits/stdc++.h"
+#pragma GCC optimize ("O3")
+#pragma GCC target ("sse4")
+ 
+using namespace std;
+ 
+typedef long long LL;
+typedef pair<int, int> PII;
+ 
+#define VE vector
+#define AR array
+ 
+#define FOR(i, A, B) for (int i = A; i < B; i++)
+#define F0R(i, N) FOR(i, 0, N)
+#define RFOR(i, A, B) for (int i = B - 1; i >= A; i--)
+#define RF0R(i, N) RFOR(i, 0, N)
+#define REP(N) F0R(_, N)
+ 
+#define SZ(X) ((int)(X).size())
+#define ALL(X) (X).begin(), (X).end()
+#define RALL(X) (X).rbegin(), (X).rend()
+#define F first
+#define S second
+ 
+constexpr LL cost = 1e12 + 1;
+ 
+void solve() {
+    string S;
+    cin >> S;
+    int N = SZ(S);
+    VE<int> pref(N + 1);
+    F0R(i, N) {
+        pref[i + 1] = pref[i] + (S[i] == '0');
+    }
+    VE<int> suf(N + 1);
+    RF0R(i, N) {
+        suf[i] = suf[i + 1] + (S[i] == '1');
+    }
+    int keep = 0;
+    bool swap = 0;
+    F0R(i, N + 1) {
+        keep = max(keep, pref[i] + suf[i]);
+    }
+    F0R(i, N - 1) {
+        if (S[i] == '1' && S[i + 1] == '0' && pref[i] + suf[i + 2] == keep - 1) {
+            swap = 1;
+            break;
+        }
+    }
+    cout << (N - keep) * cost - swap << '\n';
+}
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int T = 1;
+    cin >> T;
+    while (T--) {
+        solve();
+    }
+}
+ 
+// Read the statement carefully, take your time
+// Don't be intimidated, the solution is probably simpler than you think
+// CM soon :pray:
