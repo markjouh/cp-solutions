1f11543455397bbfed2cac302926b3f67bc6acd1
Mark Zhou <xinyunzhouu@gmail.com>
2023-08-13 13:13:59 -0400

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1849A | Morning Sandwich/215885887.cpp
@@ -0,0 +1,29 @@
+#include "bits/stdc++.h"
+using namespace std;
+ 
+typedef long long ll;
+typedef pair<int, int> pii;
+ 
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
+ 
+template<class T> bool ckmin(T &a, const T &b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool ckmax(T &a, const T &b) { return b > a ? a = b, 1 : 0; }
+ 
+void solve() {
+  int b, c, h;
+  cin >> b >> c >> h;
+  c += h;
+  cout << min(b - 1, c) * 2 + 1 << '\n';
+}
+ 
+int main() {
+  cin.tie(0)->sync_with_stdio(0);
+  cin.exceptions(cin.failbit);
+  int t = 1;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+  return 0;
+}
